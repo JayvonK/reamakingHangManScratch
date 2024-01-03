c4a98ec259bcf648edb9b2199004cf7eaaeb33f5
JayvonK <jaykhuth@gmail.com>
2024-01-03 06:33:19 -0800

    restartBtn

--- scripts/app.js
+++ scripts/app.js
@@ -19,6 +19,25 @@ restartBtn.addEventListener('click', function(e){
     RestartGame();
 })
 
+restartBtn.addEventListener('click', function(e){
+    guesses = 0;
+    maxGusses = 5;
+    randomWord = "";
+    wrongGuess = "";
+    displayedWord = [];
+    hangMan.innerText = "Guesses Left / Max Guesses";
+    userInput.value = "";
+    userInput.readOnly = true;
+    wrongGuesses.innerText = "Wrong Guesses";
+    secretWord.innerText = "[Secret Word]";
+})
+
 async function ApiCall() {
-    const promise = await fetch('')
+    const promise = await fetch('https://random-word-api.herokuapp.com/word')
+    const data = await promise.json();
+
+    StartGame(data[0]);
 }
+
+
+

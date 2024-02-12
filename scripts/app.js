99c0e5c28536f63c3d9275e1311311313bf60143
JayvonK <jaykhuth@gmail.com>
2024-02-11 22:42:42 -0800

    app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,49 @@
 // 'https://random-word-api.herokuapp.com/word'
+let startBtn = document.getElementById("startBtn");
+let resetBtn = document.getElementById("resetBtn");
+let secretWord = document.getElementById("secretWord");
+let wrongGuesses = document.getElementById("wrongGuesses");
+let hangMan = document.getElementById("hangMan");
+let userInput = document.getElementById("userInput");
+
+let display = [];
+let word = "";
+let guesses = 0;
+let guessesLeft = 5;
+let start = true;
+
+const GetWord = async () => {
+    const promise = await fetch('https://random-word-api.herokuapp.com/word');
+    const data = await promise.json();
+    word = data[0];
+}
+
+startBtn.addEventListener('click', () => {
+    StartGame();
+
+})
+
+resetBtn.addEventListener('click', () => {
+    ResetGame();
+})
+
+const StartGame = async () => {
+    if (start) {
+        await GetWord();
+        word.split("").forEach(el => display.push('_'));
+        secretWord.textContent = display.join(" ");
+        hangMan.textContent = `Guesses: ${guesses} / ${guessesLeft}`;
+        userInput.readOnly = false;
+    }
+    start = false;
+}
+
+const ResetGame = () => {
+
+}
+
+const UpdateGame = () => {
+    for(let i = 0; i < word.length; i++){
+        
+    }
+}
